34b75bd002e4f7a4c7dee53210f2359353e0d333
Erick <gonzalezerick466@gmail.com>
2025-06-04 08:57:14 -0400

    refactored user

--- src/main/java/com/codewithmosh/store/users/UserService.java
+++ src/main/java/com/codewithmosh/store/users/UserService.java
@@ -1,26 +1,80 @@
 package com.codewithmosh.store.users;
 
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService {
     private final UserRepository userRepository;
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        var user = userRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("User not found"));
-        return new User(
-                user.getEmail(),
-                user.getPassword(),
-                List.of()
-        );
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<UserDto> getAllUsers(String sortBy) {
+        if(!Set.of("name", "email").contains(sortBy)) {
+            sortBy = "name";
+        }
+        return userRepository.findAll(Sort.by(sortBy)).stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    public UserDto getUser(Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        return userMapper.toDto(user);
+    }
+
+    public UserDto registerUser(RegisterUserRequest request) {
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistException("Email is already registered.");
+        }
+
+        var user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+
+        return userMapper.toDto(user);
+    }
+
+    public UserDto updateUser(Long id, UpdateUserRequest request) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+
+        userMapper.update(request, user);
+        userRepository.save(user);
+        return userMapper.toDto(user);
+    }
+
+    public void deleteUser(Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        userRepository.delete(user);
+    }
+
+    public void changePassword(Long id, ChangePasswordRequest request) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        if(!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
+            throw new AccessDeniedException("Password do not match.");
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
     }
 }
