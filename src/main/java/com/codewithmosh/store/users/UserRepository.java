55372aadd65e79d393f51b60abef7d1df19a3a14
Erick <gonzalezerick466@gmail.com>
2025-06-05 13:54:56 -0400

    Modularizing security rules

--- src/main/java/com/codewithmosh/store/users/UserRepository.java
+++ src/main/java/com/codewithmosh/store/users/UserRepository.java
@@ -1,10 +1,12 @@
 package com.codewithmosh.store.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 
+@Component
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
